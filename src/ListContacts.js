21ccf2b9cd38ddd25a0c856a59b7a588160d4961
David Skaggs <davidtskaggs@gmail.com>
2018-01-29 18:03:18 -0500

    Refactor stateless component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,34 @@
-import React from 'react'
+import React, { Component }from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts (props) {
-  return (
-    <ol className='contact-list'>
-      {props.contacts.map((contact) => (
-        <li key={contact.id} className='contact-list-item'>
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
 
-          <div className='contact-avatar'  style={{
-            backgroundImage: `url(${contact.avatarURL})`
-          }}/>
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
-          </div>
-          <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  )
+  render() {
+    return (
+      <ol className='contact-list'>
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id} className='contact-list-item'>
+
+            <div className='contact-avatar'  style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
 }
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-}
 
 export default ListContacts
