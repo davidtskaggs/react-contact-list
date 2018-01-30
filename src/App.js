f53a3395b0b53336a0b365eeb0bb6bdf7e926558
David Skaggs <davidtskaggs@gmail.com>
2018-01-29 23:18:46 -0500

    Update react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list',
     contacts: []
   }
   componentDidMount() {
@@ -24,18 +24,13 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        {this.state.screen === 'list' && (
+        <Route exact path="/"  render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
-            onNavigate={() => {
-              this.setState({screen: 'create'})
-            }}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
+        )} />
+        <Route path="/create" component={CreateContact}/>
       </div>
 
     )
