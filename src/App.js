b26da27787616214273b1d9672802508440ab144
David Skaggs <davidtskaggs@gmail.com>
2018-01-30 10:50:52 -0500

    Update server with form submit

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }))
+    })
+  }
+
   render() {
     return (
       <div className='app'>
@@ -30,7 +39,14 @@ class App extends Component {
             contacts={this.state.contacts}
           />
         )} />
-        <Route path="/create" component={CreateContact}/>
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
 
     )
